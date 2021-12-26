b6e7033b01553c278426314b16a862f9d0d2331b
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2021-12-26 20:10:59 +0200

    add js task 1

---
+++ javascript/task 1/1/external.js
@@ -0,0 +1,38 @@
+alert("Rana");
+
+console.log(number1)
+
+
+var number1 = 3;
+console.log(number1)
+var number2 = 2.9;
+var number3 = 0xff;
+
+var firstName = "Rana";
+var middleName = "Wagdi";
+var lastName = "Zakzouk";
+var numbers = [1, 2 ,5 ,"Rana"];
+
+var flag = true;
+
+firstName[3]="v"
+console.log("mychange name "+firstName)
+
+
+
+///type of
+console.log("typeof first Name " + typeof(firstName) +" typeof middle Name " + typeof(middleName)+" typeof last NAME " + typeof(lastName) +" type of number " + typeof(numbers)+" type of flag " + typeof(flag));
+
+
+///copy name
+console.log("my name is" + firstName +' Middle Name is ' + middleName +` last Name is ${lastName}`);
+
+
+
+console.log("This is external javascript file");
+
+
+///
+// d-get undefine
+// e-  number is change value but when refresh is not
+//f- dont chane
